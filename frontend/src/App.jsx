4764a4e06a9418d412bd8b2ea5a159e4bbbc9bb4
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-07-15 07:41:47 +0400

    add form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,65 @@
-import TaskCard from './components/taskcard.jsx'
-
-function App() {
-
+import React, { useState } from 'react';
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Radio, Tag } from 'antd';
+const customizeRequiredMark = (label, { required }) => (
+  <>
+    {required ? <Tag color="error">Required</Tag> : <Tag color="warning">optional</Tag>}
+    {label}
+  </>
+);
+const App = () => {
+  const [form] = Form.useForm();
+  const [requiredMark, setRequiredMarkType] = useState('optional');
+  const onRequiredTypeChange = ({ requiredMarkValue }) => {
+    setRequiredMarkType(requiredMarkValue);
+  };
   return (
-    <div>
-      <TaskCard/>
-    </div>
-  )
-}
+    <Form
+      form={form}
+      layout="vertical"
+      initialValues={{
+        requiredMarkValue: requiredMark,
+      }}
+      onValuesChange={onRequiredTypeChange}
+      requiredMark={requiredMark === 'customize' ? customizeRequiredMark : requiredMark}
+    >
+      <Form.Item label="Required Mark" name="requiredMarkValue">
+        <Radio.Group>
+          <Radio.Button value>Default</Radio.Button>
+          <Radio.Button value="optional">Optional</Radio.Button>
+          <Radio.Button value={false}>Hidden</Radio.Button>
+          <Radio.Button value="customize">Customize</Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item label="Field A" required tooltip="This is a required field">
+        <Input placeholder="input placeholder" />
+      </Form.Item>
+      <Form.Item
+        label="Field B"
+        tooltip={{
+          title: 'Tooltip with customize icon',
+          icon: <InfoCircleOutlined />,
+        }}
+      >
+        <Input placeholder="input placeholder" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary">Submit</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default App;
 
-export default App
+// import TaskCard from './components/taskcard.jsx'
+
+// function App() {
+
+//   return (
+//     <div>
+//       <TaskCard/>
+//     </div>
+//   )
+// }
+
+// export default App
--- frontend/src/components/taskcard.jsx
+++ frontend/src/components/taskcard.jsx
@@ -1,20 +1,51 @@
-import { Card } from 'antd';
+import React, { useState } from 'react';
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Radio, Tag } from 'antd';
+const customizeRequiredMark = (label, { required }) => (
+  <>
+    {required ? <Tag color="error">Required</Tag> : <Tag color="warning">optional</Tag>}
+    {label}
+  </>
+);
+
 
 function TaskCard() {
 
     return (
       <>
-        <Card
-      title="Task"
-      extra={<a href="#">More</a>}
-      style={{
-        width: 300,
+        <Form
+      form={form}
+      layout="vertical"
+      initialValues={{
+        requiredMarkValue: requiredMark,
       }}
+      onValuesChange={onRequiredTypeChange}
+      requiredMark={requiredMark === 'customize' ? customizeRequiredMark : requiredMark}
     >
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
+      <Form.Item label="Required Mark" name="requiredMarkValue">
+        <Radio.Group>
+          <Radio.Button value>Default</Radio.Button>
+          <Radio.Button value="optional">Optional</Radio.Button>
+          <Radio.Button value={false}>Hidden</Radio.Button>
+          <Radio.Button value="customize">Customize</Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item label="Field A" required tooltip="This is a required field">
+        <Input placeholder="input placeholder" />
+      </Form.Item>
+      <Form.Item
+        label="Field B"
+        tooltip={{
+          title: 'Tooltip with customize icon',
+          icon: <InfoCircleOutlined />,
+        }}
+      >
+        <Input placeholder="input placeholder" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary">Submit</Button>
+      </Form.Item>
+    </Form>
       </>
     )
   }
